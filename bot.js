98f151baee5d94773ead8863437ee8e6c19b1d70
skabir8 <skabir@stuy.edu>
2018-06-10 19:02:53 -0400

    finished

--- bot.js
+++ bot.js
@@ -1,85 +1,152 @@
 
 console.log('start')
 const request = require('request');
-var unirest = require('unirest');
 
 var Twit = require('twit')
 
 var config = require('./config');
 var T = new Twit(config);
-var games = []
-
-
+var my_data = {
+  "genres": [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+}
 
 var makeRun = function(){
   var stream = T.stream('statuses/filter', { track: ['@ShariarKabir14'] });
   stream.on('tweet', tweetEvent);
   function tweetEvent(tweet) {
-    console.log("lets start");
+    console.log("heyyy");
     var name = tweet.user.screen_name;
     var my_user_id = tweet.user.id;
     var txt = tweet.text;
     var nameID  = tweet.id_str;
-    var search_query = "http://poetrydb.org/lines/" + "shall%20i%20compare";
+    var search_query = "https://api.themoviedb.org/3/";
     var reply = "";
     var data = {authors: [], titles: []}
     var response;
-    request(search_query, { json: true }, (err, res, body) => {
-      response = res.body;
-      if (response != null){
-        for (i = 0; i < 3; i++){
-          console.log('adasdasd')
-          var poem_selection_index = Math.floor(Math.random() * (response.length - 0 + 1)) + 0;
-          if (response[poem_selection_index] != null){
-            var poem_selection = response[poem_selection_index]['lines'];
-            for (j = 0; j < poem_selection.length; j++){
-              var my_sentence = poem_selection[j].toLowerCase();
-              if (my_sentence.indexOf("Shall I compare".toLowerCase()) != -1){
-                data['authors'].push(response[poem_selection_index]['author']);
-                data['titles'].push(response[poem_selection_index]['title']);
-                reply += my_sentence + '\n';
-              }
-              j = poem_selection.length + 1;
-            }
-          }
-          else{
-            i--;
-          }
+    var og = txt;
+    if (txt.indexOf("genre") != -1){
+      txt = txt.substring(txt.indexOf(" ") + 1,txt.length);
+      txt = txt.substring(6, txt.length);
+      var my_genre = txt.toLowerCase();
+      var my_genre_num = 0;
+      for (i = 0; i < my_data['genres'].length; i++){
+        if ((my_data['genres'][i]['name']).toLowerCase() == my_genre){
+          my_genre_num = my_data['genres'][i]['id'];
+          i = my_data['genres'].length + 1;
         }
-        console.log(reply, data['authors'], data['titles']);
       }
-    });
-    /*
-    T.post("direct_messages/new",{  screen_name	: name, text: 'YOUR_REPLY'}, function(err, data, response){
-      console.log("sent")
+      search_query += "discover/movie?api_key=65a3cdc61a25fc687d858ca991f83a4a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres="+ my_genre_num;
+    }
+    else if (txt.indexOf("person") != -1){
+      txt = txt.substring(txt.indexOf(" ") + 1,txt.length);
+      txt = txt.substring(7, txt.length);
+      search_query += "search/person?api_key=65a3cdc61a25fc687d858ca991f83a4a&query=" + txt;
+    }
+    else{
+      txt = txt.substring(txt.indexOf(" ") + 1,txt.length);
+      search_query += "search/movie?api_key=65a3cdc61a25fc687d858ca991f83a4a&query=" + txt;
+    }
+    if (og.toLowerCase().indexOf('person'.toLowerCase()) == -1){
+      request(search_query, { json: true }, (err, res, body) => {
+        response = res.body['results'];
+        var my_movie = response[Math.floor(Math.random() * response.length)];;
+        var my_movie_id = my_movie['id'];
+        var reply = "https://www.themoviedb.org/movie/" + my_movie_id;
+        T.post('statuses/update', { status: reply }, function(err, data, response) {
+          console.log(data)
+        })
+      });
+    }
+    else{
+      request(search_query, { json: true }, (err, res, body) => {
+        response = res.body['results'][0]['known_for'];
+        var my_movie = response[Math.floor(Math.random() * response.length)];;
+        var my_movie_id = my_movie['id'];
+        var reply = "https://www.themoviedb.org/movie/" + my_movie_id;
+        T.post('statuses/update', { status: reply }, function(err, data, response) {
+          console.log(data)
+        })
+      });
 
-    });
-
-
-
-    var reply = "You mentioned me! @" + name + ' ' + 'You are super cool!';
-    var params             = {
-      status: reply,
-      in_reply_to_status_id: nameID
-    };
-
-    T.post('statuses/update', params, function(err, data, response) {
-      if (err !== undefined) {
-        console.log(err);
-      } else {
-        console.log('Tweeted: ' + params.status);
-      }
-    })*/
-
-
+    }
   };
-  /*
-  T.post('statuses/update', { status: "I'm still not a robot boi" }, function(err, data, response) {
-    console.log(data)
-
-  })*/
-
 }
-//my_id('ShariarKabir14')
-//get_userID('shkabir8');
+
 setInterval(makeRun, 4000);
