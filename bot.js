fb353cb516d330524f477aa05d2305498a4f93b8
skabir8 <skabir@stuy.edu>
2018-06-10 16:57:30 -0400

    added

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 
 console.log('start')
+const request = require('request');
+var unirest = require('unirest');
+
 var Twit = require('twit')
 
 var config = require('./config');
@@ -11,31 +14,55 @@ var games = []
 var makeRun = function(){
   var stream = T.stream('statuses/filter', { track: ['@ShariarKabir14'] });
   stream.on('tweet', tweetEvent);
-  //stream.on('message', function (msg) {
-  //  console.log('u got a message');
-
-  //})
   function tweetEvent(tweet) {
-    console.log("heyyy");
-    // Who sent the tweet?
+    console.log("lets start");
     var name = tweet.user.screen_name;
     var my_user_id = tweet.user.id;
-    // What is the text?
-    // var txt = tweet.text;
-    // the status update or tweet ID in which we will reply
+    var txt = tweet.text;
     var nameID  = tweet.id_str;
-
+    var search_query = "http://poetrydb.org/lines/" + "shall%20i%20compare";
+    var reply = "";
+    var data = {authors: [], titles: []}
+    var response;
+    request(search_query, { json: true }, (err, res, body) => {
+      response = res.body;
+      if (response != null){
+        for (i = 0; i < 3; i++){
+          console.log('adasdasd')
+          var poem_selection_index = Math.floor(Math.random() * (response.length - 0 + 1)) + 0;
+          if (response[poem_selection_index] != null){
+            var poem_selection = response[poem_selection_index]['lines'];
+            for (j = 0; j < poem_selection.length; j++){
+              var my_sentence = poem_selection[j].toLowerCase();
+              if (my_sentence.indexOf("Shall I compare".toLowerCase()) != -1){
+                data['authors'].push(response[poem_selection_index]['author']);
+                data['titles'].push(response[poem_selection_index]['title']);
+                reply += my_sentence + '\n';
+              }
+              j = poem_selection.length + 1;
+            }
+          }
+          else{
+            i--;
+          }
+        }
+        console.log(reply, data['authors'], data['titles']);
+      }
+    });
+    /*
     T.post("direct_messages/new",{  screen_name	: name, text: 'YOUR_REPLY'}, function(err, data, response){
       console.log("sent")
 
     });
+
+
 
     var reply = "You mentioned me! @" + name + ' ' + 'You are super cool!';
     var params             = {
       status: reply,
       in_reply_to_status_id: nameID
     };
-    /*
+
     T.post('statuses/update', params, function(err, data, response) {
       if (err !== undefined) {
         console.log(err);
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         'LNeAWwHFIX9U14D1YJK6uHjLs',
+  consumer_secret:      'n7rZIfZktfWjvwKk0VuRy5ni5JwcISuHdGcjLcBjnMnnc2hKr4',
+  access_token:         '1005105652396589057-FXdkm0REXCFKTmsoJFRT3TmMly8jNG',
+  access_token_secret:  'jqDOCOotYTrs5lYzpPSDvklPM3wxICuT9fSW4Hcc46H0V',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+};
